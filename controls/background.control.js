cde10e9791ee10a213c9d506e11324c4f88cc125
joalra <johannes.rasinger@gmx.at>
2016-11-19 17:20:34 +0100

    bg

---
+++ controls/background.control.js
@@ -0,0 +1,21 @@
+export default class {
+  static parameters() {
+    return {
+    };
+  }
+
+  static styleUrls() {
+     return [
+       'top-office/style.css'
+     ];
+   }
+
+   static template() {
+      return `
+        <div class="background"></div>
+      `;
+    }
+
+  createdCallback() {
+  }
+}
